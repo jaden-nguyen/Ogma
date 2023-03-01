689d87c7e41a7566e99521663f3ff59b62232ed9
Jaden Nguyen <jadenthai206@gmail.com>
2023-03-01 14:00:54 -0800

    password regex working

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -8,6 +8,8 @@ const Login: React.FC<LoginProps> = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
   const [username, setUsername] = useState<User | null>(userObj);
   const [password, setPassword] = useState<User | null>(userObj);
+  const [categories, setCategories] = useState(false);
+  const [checkPass, setCheckPass] = useState(false);
 
   const handleUser = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername((prevUser) => ({ ...prevUser, username: event.target.value }));
@@ -15,14 +17,40 @@ const Login: React.FC<LoginProps> = ({ setIsLoggedIn }) => {
   };
 
   const handlePass = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword((prevUser) => ({ ...prevUser, password: event.target.value }));
-    console.log(password.password);
+      setPassword((prevUser) => ({ ...prevUser, password: event.target.value }));
+      
+      
+      const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).+$/;
+      if (regex.test(password.password)) setCategories(true);
+      else setCategories(false);
+      
+      if (username.username.length > 1 && 
+        password.password !== username.username) setCheckPass(true);
+      else setCheckPass(false);
+
+      console.log(password.password);
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === 'Space') {
+      event.preventDefault();
+    }
+  }
+
   const handleLogIn = () => {
-    console.log("logged in");
-    setIsLoggedIn(true);
-    navigate("/home");
+    console.log("attempting login");
+    if (!(categories && checkPass && password.password.length >= 8)) {
+      alert('Please fill out all of the password requirements.');
+    } 
+    // else {
+
+    // perform fetch 
+
+    // if user is verified
+      setIsLoggedIn(true);
+      navigate("/home");
+
+    // }
   };
 
   return (
@@ -35,6 +63,7 @@ const Login: React.FC<LoginProps> = ({ setIsLoggedIn }) => {
           id="username"
           value={username.username}
           onChange={(e) => handleUser(e)}
+          onKeyDown={handleKeyDown}
         ></input>
         <label htmlFor="password">Password</label>
         <input
@@ -43,7 +72,18 @@ const Login: React.FC<LoginProps> = ({ setIsLoggedIn }) => {
           id="password"
           value={password.password}
           onChange={(e) => handlePass(e)}
+          onKeyDown={handleKeyDown}
         ></input>
+      </section>
+      <section className="password-requirements">
+        <p id="pass-categories" style={{ color: categories ? passStyleActive.color: passStyle.color }}>
+            Your password must use characters from at all of these categories: uppercase letters, lowercase letters, numbers, symbols.</p>
+        <p id="pass-length" 
+          style={{ color: password.password.length >= 8 ? passStyleActive.color: passStyle.color }}>
+            Your password must at least 8 characters long.
+        </p>
+        <p id="pass-name" style={{ color: checkPass ? passStyleActive.color: passStyle.color }}>
+            Your password cannot contain your name (or a portion of your name).</p>
       </section>
       <div className="button-container">
         <button className="login-btn" onClick={handleLogIn}>
@@ -61,5 +101,15 @@ const userObj = {
   username: "",
   password: "",
 };
+
+const passStyle = {
+  color: "#FF0000", //red
+  transition: "all 0.3s ease-in-out",
+}
+
+const passStyleActive = {
+  color: "#90EE90", //green
+  transition: "all 0.3s ease-in-out",
+}
 
 export default Login;
